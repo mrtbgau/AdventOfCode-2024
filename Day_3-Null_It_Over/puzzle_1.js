3011acfdcb73dc20b4fb7c5a2bbfdfd33e07cf43
mrtbgau <mrt.bgau@gmail.com>
2024-12-04 22:17:21 +0100

    calculate mutliplications results

--- Day_3-Null_It_Over/puzzle_1.js
+++ Day_3-Null_It_Over/puzzle_1.js
@@ -1,11 +1,25 @@
-import { log } from "console";
-
 const input = Bun.file("input.txt");
 const inputContent = await input.text();
 
 const regex = /mul\((\d{1,3}),(\d{1,3})\)/g;
 
+let multiplications = [];
+
+let allResults = 0;
+
 const getAllDigitsToMultilply = () => {
   const matches = [...inputContent.matchAll(regex)];
-  return matches.map((match) => [parseInt(match[1]), parseInt(match[2])]);
+  multiplications = matches.map((match) => [
+    parseInt(match[1]),
+    parseInt(match[2]),
+  ]);
 };
+
+getAllDigitsToMultilply();
+
+multiplications.forEach((mul) => {
+  const result = mul.reduce((a, b) => a * b, 1);
+  allResults += result;
+});
+
+console.log(allResults);
