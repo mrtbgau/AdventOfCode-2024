2d2756b4170a76606e8bccdc4fbf4c9c2cfc306d
mrtbgau <mrt.bgau@gmail.com>
2024-12-12 00:06:21 +0100

    get correct updates

---
+++ Day_5-Print_Queue/puzzle_1.js
@@ -0,0 +1,46 @@
+const input = Bun.file("input.txt");
+const inputContent = await input.text();
+const inputLines = inputContent.split("\n");
+
+const rules = [];
+const updates = [];
+const correctUpdates = [];
+const ruleRegEx = /^([0-9]{2})\|([0-9]{2})$/;
+
+let isCorrect = false;
+
+inputLines.forEach((line) => {
+  line = line.replace("\r", "");
+  const match = line.match(ruleRegEx);
+  if (line.match(ruleRegEx) !== null) {
+    rules.push([parseInt(match[1]), parseInt(match[2])]);
+  } else {
+    updates.push(line);
+  }
+});
+
+for (let u = 0; u < updates.length; u++) {
+  let isUpdateCorrect = true;
+
+  for (let r = 0; r < rules.length; r++) {
+    if (
+      updates[u].includes(rules[r][0].toString()) &&
+      updates[u].includes(rules[r][1].toString())
+    ) {
+      const updatePages = updates[u].split(",");
+      const beforeIndex = updatePages.indexOf(rules[r][0].toString());
+      const afterIndex = updatePages.indexOf(rules[r][1].toString());
+
+      if (beforeIndex > afterIndex) {
+        isUpdateCorrect = false;
+        break;
+      }
+    }
+  }
+
+  if (isUpdateCorrect) {
+    correctUpdates.push(updates[u]);
+  }
+}
+
+console.log(correctUpdates);
